7d069ccb642f52f3e8999c6c710fe9baf1f9a9d4
Etuyxz <a0933679306@gmail.com>
2026-04-23 16:28:18 +0700

    A3_019 v2

--- TOI_ZERO/A3_019.py
+++ TOI_ZERO/A3_019.py
@@ -2,20 +2,16 @@ import sys
 input = sys.stdin.readline
 n,l = map(int,input().split())
 q = []
-add = []
 for _,x in zip(range(n),map(int,input().split())):
     q.append(x)
     
 for _,y in zip(range(l),map(int,input().split())):
     ry = y-1
     if ry == 0:
-        add.append(0)
+        print(0)
     else:
         arr = max([q[j] for j in range(ry)])
         if q[ry] <= arr:
-            add.append(arr+1-q[ry])
+            print(arr+1-q[ry])
         else:
-            add.append(0)
-
-for k in add:
-    print(k)
+            print(0)
