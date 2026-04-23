7393e36539fcc1c0c417ea9b0f19ac991fff7b58
Etuyxz <a0933679306@gmail.com>
2026-04-23 16:17:26 +0700

    new A3_019

--- TOI_ZERO/A3_019.py
+++ TOI_ZERO/A3_019.py
@@ -2,24 +2,20 @@ import sys
 input = sys.stdin.readline
 n,l = map(int,input().split())
 q = []
-chair = []
+add = []
 for _,x in zip(range(n),map(int,input().split())):
     q.append(x)
     
 for _,y in zip(range(l),map(int,input().split())):
-    chair.append(y-1)
-    
-add = []
-for i in chair:
-    if i == 0:
+    ry = y-1
+    if ry == 0:
         add.append(0)
     else:
-        arr = max([q[j] for j in range(i)])
-        if q[i] <= arr:
-            add.append(arr+1-q[i])
+        arr = max([q[j] for j in range(ry)])
+        if q[ry] <= arr:
+            add.append(arr+1-q[ry])
         else:
             add.append(0)
-
 
 for k in add:
     print(k)
